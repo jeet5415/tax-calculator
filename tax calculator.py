ca73ca161a5e766c84e9869fb5f4c53cb6597450
jeet5415 <117734319+jeet5415@users.noreply.github.com>
2026-07-01 21:27:13 +0530

    Update tax calculator.py

--- tax calculator.py
+++ tax calculator.py
@@ -1,29 +1,31 @@
 def calculate_tax(taxable_income):
     tax = 0
 
- if taxable_income <= 800000:
-        tax = (taxable_income - 400000) * 0.05
+    if taxable_income > 400000:
 
-    elif taxable_income <= 1200000:
-        tax = 20000 + (taxable_income - 800000) * 0.10
+        if taxable_income <= 800000:
+            tax = (taxable_income - 400000) * 0.05
 
-    elif taxable_income <= 1600000:
-        tax = 60000 + (taxable_income - 1200000) * 0.15
+        elif taxable_income <= 1200000:
+            tax = 20000 + (taxable_income - 800000) * 0.10
 
-    elif taxable_income <= 2000000:
-        tax = 120000 + (taxable_income - 1600000) * 0.20
+        elif taxable_income <= 1600000:
+            tax = 60000 + (taxable_income - 1200000) * 0.15
 
-    elif taxable_income <= 2400000:
-        tax = 200000 + (taxable_income - 2000000) * 0.25
+        elif taxable_income <= 2000000:
+            tax = 120000 + (taxable_income - 1600000) * 0.20
 
-    else:
-        tax = 300000 + (taxable_income - 2400000) * 0.30
+        elif taxable_income <= 2400000:
+            tax = 200000 + (taxable_income - 2000000) * 0.25
+
+        else:
+            tax = 300000 + (taxable_income - 2400000) * 0.30
 
     return tax
 
 
 def main():
-    gross_salary = 1300000
+    gross_salary = 700000
     standard_deduction = 40000
     deduction_80c = 75000
     tds_paid =40000
