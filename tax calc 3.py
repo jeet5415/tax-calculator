3b740b2c1f41f49ad501ba36227694782a805a98
jeet5415 <117734319+jeet5415@users.noreply.github.com>
2026-07-04 16:48:20 +0530

    Refactor tax calculation and update main parameters
    Updated gross salary and TDS values, modified print statements for clarity, and adjusted tax calculation logic.

--- tax calc 3.py
+++ tax calc 3.py
@@ -1,21 +1,20 @@
 def calculate_tax(x):
     if x <= 1200000:
-        return 0   # rebate 87A
+        return 0   # Section 87A
 
-    tax = ( max(0, min(x, 800000) - 400000) * 0.05 +
-        max(0, min(x, 1200000) - 800000) * 0.10 +
-        max(0, min(x, 1600000) - 1200000) * 0.15 +
-        max(0, min(x, 2000000) - 1600000) * 0.20 +
-        max(0, min(x, 2400000) - 2000000) * 0.25 +
-        max(0, x - 2400000) * 0.30)
+    tax = ((max(0, min(x, 800000) - 400000) * 0.05) +
+        (max(0, min(x, 1200000) - 800000) * 0.10) +
+        (max(0, min(x, 1600000) - 1200000) * 0.15) +
+        (max(0, min(x, 2000000) - 1600000) * 0.20) +
+        (max(0, min(x, 2400000) - 2000000) * 0.25) +
+        (max(0, x - 2400000) * 0.30))
 
     return tax
 
-
 def main():
-    gross_salary = 1210000
+    gross_salary = 1400000
     standard_deduction = 75000
-    tds_paid = 40000
+    tds_paid = 30000
 
     total_deductions = standard_deduction
     taxable_income = max(0, gross_salary - total_deductions)
@@ -26,16 +25,13 @@ def main():
     balance = total_tax - tds_paid
 
     print("Gross Salary: ₹", gross_salary)
-    print("Total Deductions: ₹", total_deductions)
     print("Taxable Income: ₹", taxable_income)
-    print("Tax: ₹", tax)
-    print("Cess: ₹", cess)
-    print("Total Tax: ₹", total_tax)
+    print("Income Tax: ₹", tax)
+    print("Health & Education Cess (4%): ₹", cess)
+    print("Total Tax need to be paid: ₹", total_tax)
 
     if balance > 0:
-        print("Tax need to be paid: ₹", balance)
+        print("Additional Tax need to be paid: ₹", balance)
     else:
-        print("Refund: ₹", abs(balance))
-
-
+        print("Refund Amount: ₹", abs(balance))
 main()
